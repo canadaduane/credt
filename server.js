39d771cfed7e716744ce279412dbc5e173b0dd03
Duane Johnson <duane.johnson@gmail.com>
2023-06-18 20:24:38 -0600

    Use uWebSockets directly for now

--- server.js
+++ server.js
@@ -1,41 +1,22 @@
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import startServer from "chef-uws";
+import uws, { DEDICATED_COMPRESSOR_3KB } from "uWebSockets.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const appPath = join(__dirname, "build");
+uws
+  .App()
+  .ws("/*", {
+    compression: DEDICATED_COMPRESSOR_3KB,
 
-/**
- * @type {Global.WSPlugin}
- */
-const shelfMergePlugin = function (ws, { id, event, data }) {
-  // broadcast to all sockets inside this topic
-  console.log({ this: this });
-  this.to("shelf").emit(event, id, data);
-};
-
-const server = await startServer({
-  debug: process.argv.includes("--debug"),
-  port: Number(process.env.PORT || 4200),
-  plugins: {
-    shelf: shelfMergePlugin,
-  },
-  join: "/join",
-  leave: "/leave",
-  folder: appPath, //static files
-  maxCacheSize: 0,
-});
-
-server.any(
-  "/*",
-
-  /**
-   *
-   * @param {Global.HttpResponse} res
-   * @param {Global.HttpRequest} req
-   */
-  (res, req) => {
-    res.end("200 OK");
-  }
-);
+    message: (ws, message, isBinary) => {
+      let ok = ws.send(message, isBinary, true);
+    },
+  })
+  .get("/*", (res, req) => {
+    res
+      .writeStatus("200 OK")
+      .writeHeader("IsExample", "Yes")
+      .end("Hello there!");
+  })
+  .listen(9001, (listenSocket) => {
+    if (listenSocket) {
+      console.log("Listening to port 9001");
+    }
+  });
